bc4269dbe2bb786edf1a998cf2e060477cab51d8
Victor Zhao <victorzhao1999@gmail.com>
2019-05-15 03:47:59 +0100

    Add transition effect from WeatherInfoPage to HomePage; Add visual effect on temp unit labels

--- src/iSport/Frames/HomePage/HomePageController.java
+++ src/iSport/Frames/HomePage/HomePageController.java
@@ -9,10 +9,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -23,32 +26,47 @@ public class HomePageController implements Initializable {
 
     @FXML
     private Button burgerButton;
-
-    @FXML
-    private AnchorPane anchorHomePage;
-
     @FXML
     private StackPane masterContainer;
+    @FXML
+    private Label celsiusLabel;
+    @FXML
+    private Label fahrenheitLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        // TODO: load weather information from the weather API
     }
 
     @FXML
     private void loadWeatherInfoPage(MouseEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("src/iSport/Frames/WeatherInfoPage/WeatherInfoPage.fxml"));
+        Parent anchorWeatherInfo = FXMLLoader.load(getClass().getClassLoader().getResource("src/iSport/Frames/WeatherInfoPage/WeatherInfoPage.fxml"));
+        Scene scene = burgerButton.getScene();
 
-        root.translateXProperty().set(-anchorHomePage.getWidth());
-        masterContainer.getChildren().add(root);
+        anchorWeatherInfo.translateXProperty().set(-scene.getWidth());
+        masterContainer.getChildren().add(anchorWeatherInfo);
 
         Timeline timeline = new Timeline();
-        KeyValue kv = new KeyValue(root.translateXProperty(), 0, Interpolator.EASE_IN);
+        KeyValue kv = new KeyValue(anchorWeatherInfo.translateXProperty(), 0, Interpolator.EASE_IN);
         KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
         timeline.getKeyFrames().add(kf);
-        timeline.setOnFinished(e -> {
-            masterContainer.getChildren().remove(kf);
-        });
         timeline.play();
+    }
+
+    @FXML
+    private void switchTempUnit(MouseEvent event) {
+        if(celsiusLabel.getTextFill()==Color.WHITESMOKE) {
+            // TODO: change from C to F and display the new temperature values
+            celsiusLabel.setTextFill(Color.web("#b2b2b2"));
+            celsiusLabel.setFont(new Font("Consolas Bold",18));
+            fahrenheitLabel.setTextFill(Color.WHITESMOKE);
+            fahrenheitLabel.setFont(new Font("Consolas Bold",23));
+        } else {
+            // TODO: change from F to C and display the new temperature values
+            fahrenheitLabel.setTextFill(Color.web("#b2b2b2"));
+            fahrenheitLabel.setFont(new Font("Consolas Bold",18));
+            celsiusLabel.setTextFill(Color.WHITESMOKE);
+            celsiusLabel.setFont(new Font("Consolas Bold",23));
+        }
     }
 }
--- src/iSport/Frames/WeatherInfoPage/WeatherInfoPageController.java
+++ src/iSport/Frames/WeatherInfoPage/WeatherInfoPageController.java
@@ -1,15 +1,56 @@
 package src.iSport.Frames.WeatherInfoPage;
 
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class WeatherInfoPageController implements Initializable {
 
+    @FXML
+    private Label backLabel;
+    @FXML
+    private AnchorPane anchorWeatherInfo;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // TODO: load weather information from the weather API
+    }
 
+    @FXML
+    private void loadHomePage(MouseEvent event) throws IOException {
+        Parent anchorHomePage = FXMLLoader.load(getClass().getClassLoader().getResource("src/iSport/Frames/HomePage/HomePage.fxml")); // currently anchorHomePage is the StackPane
+        anchorHomePage = (Parent) ((StackPane) anchorHomePage).getChildren().get(0); // now anchorHomePage is the actual AnchorPane that we want
+        Parent current = anchorWeatherInfo;
+        Scene scene = backLabel.getScene();
+
+        current.translateXProperty().set(0);
+        StackPane masterContainer = (StackPane) scene.getRoot();
+        if (masterContainer.getChildren().contains(anchorHomePage))
+            masterContainer.getChildren().add(0, anchorHomePage);
+
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(current.translateXProperty(), -scene.getWidth(), Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(e -> {
+            masterContainer.getChildren().remove(anchorWeatherInfo);
+        });
+        timeline.play();
     }
 }
