54d8ec25340433200280eaa5aee17cdfc0c4815a
Andreea Zaharia <andreea.zaharia10@gmail.com>
2019-05-16 19:29:13 +0100

    Added SportList

---
+++ src/Preferences/SportList.java
@@ -0,0 +1,32 @@
+package src.Preferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SportList {
+    static private List<Sport> sportList = null;
+
+    public static List<Sport> get() {
+        if (sportList == null) {
+            sportList = new ArrayList<>();
+        }
+
+        return sportList;
+    }
+
+    public static void add(Sport sport) {
+        if (sportList == null) {
+            sportList = new ArrayList<>();
+        }
+
+        sportList.add(sport);
+    }
+
+    public static void remove(Sport sport) {
+        sportList.remove(sport);
+    }
+
+    public static void clear(Sport sport) {
+        sportList.clear();
+    }
+}
